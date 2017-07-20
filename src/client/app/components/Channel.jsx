297defc2d04eef4d83d8d312aa057cb175d7b570
everm1nd <everm1nd.mail@gmail.com>
2017-07-20 14:11:14 +0200

    made Channel component to play sound file

--- src/client/app/components/Channel.jsx
+++ src/client/app/components/Channel.jsx
@@ -2,6 +2,12 @@ import React from 'react';
 import { Howl } from 'howler';
 import Slider from './Slider.jsx';
 
+const playSound = (path) => {
+  const sound = new Howl({ src: path, buffer: true });
+  sound.play();
+  console.log('Playing sound', path);
+}
+
 class Channel extends React.Component {
 
   constructor(props) {
@@ -10,8 +16,7 @@ class Channel extends React.Component {
     super(props);
     this.state = { volume: props.volume || DEFAULT_VOLUME };
 
-    const sound = new Howl({ src: './assets/sound.wav', buffer: true });
-    sound.play();
+    playSound('./assets/sound.wav');
   }
 
   render() {
