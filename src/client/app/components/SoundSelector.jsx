5b3767342f3c62ec769d468a42f8e8c78f17a38d
everm1nd <everm1nd.mail@gmail.com>
2017-07-21 15:23:00 +0200

    added test for SoundSelector

--- src/client/app/components/SoundSelector.jsx
+++ src/client/app/components/SoundSelector.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const SoundSelector = (props) => {
   return(<select className="sound-selector" value={props.sound} onChange={props.onSoundChange}>
     <option value="birds.wav">Brids</option>
     <option value="rain.wav">Rain</option>
   </select>);
+}
+
+SoundSelector.propTypes = {
+  sound: PropTypes.string.isRequired,
+  onSoundChange: PropTypes.func.isRequired
 }
 
 export default SoundSelector;
