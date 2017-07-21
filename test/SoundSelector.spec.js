cb53aeb50aec6423d818dc9f5d0f0e3d3c927b69
everm1nd <everm1nd.mail@gmail.com>
2017-07-21 15:33:18 +0200

    refactored test for SoundSelector

--- test/SoundSelector.spec.js
+++ test/SoundSelector.spec.js
@@ -5,19 +5,18 @@ import { expect } from 'chai';
 import SoundSelector from '../src/client/app/components/SoundSelector.jsx';
 
 describe('<SoundSelector/>', function () {
+  const testFunction = () => { console.log('bam!') };
+  const wrapper = shallow(<SoundSelector sound="birds.wav" onSoundChange={testFunction} />);
+
   it('renders 2 options with different sounds', function () {
-    const wrapper = shallow(<SoundSelector/>);
     expect(wrapper.find('option')).to.have.length(2);
   });
 
   it('passes `sound` prop to value of select', function () {
-    const wrapper = shallow(<SoundSelector sound="birds.wav"/>);
     expect(wrapper.props().value).to.be.eql('birds.wav');
   });
 
   it('passes `onSoundChange` prop to onChange of select', function () {
-    const onSoundChange = () => { console.log('bam!') };
-    const wrapper = shallow(<SoundSelector onSoundChange={onSoundChange} />);
-    expect(wrapper.props().onChange).to.be.eql(onSoundChange);
+    expect(wrapper.props().onChange).to.be.eql(testFunction);
   });
 });
