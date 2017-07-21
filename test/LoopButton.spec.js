c9b6ae318ed8f1a89eb56e277be4c40b4106a430
everm1nd <everm1nd.mail@gmail.com>
2017-07-21 15:39:05 +0200

    added test for LoopButton

---
+++ test/LoopButton.spec.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { mount, shallow } from 'enzyme';
+import { expect } from 'chai';
+
+import LoopButton from '../src/client/app/components/LoopButton.jsx';
+
+describe('<LoopButton/>', function () {
+  const testFunction = () => { console.log('bam!') };
+  const wrapper = shallow(<LoopButton onClick={testFunction} />);
+
+  it('renders one button', function () {
+    expect(wrapper.find('input[type="button"]')).to.have.length(1);
+  });
+
+  it('passes `onClick` prop to onClick of button', function () {
+    expect(wrapper.props().onClick).to.be.eql(testFunction);
+  });
+});
