ba10de39bb9d9ebb88cde52c5a1b5f03e28247d4
everm1nd <everm1nd.mail@gmail.com>
2017-07-21 15:44:50 +0200

    added proptypes check for LoopButton

--- src/client/app/components/LoopButton.jsx
+++ src/client/app/components/LoopButton.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const LoopButton = (props) => {
   return <input type="button" value="⟳" onClick={props.onClick} />;
+}
+
+LoopButton.propTypes = {
+  onClick: PropTypes.func.isRequired
 }
 
 export default LoopButton;
