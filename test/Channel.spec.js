039beef8bd5a030b35abc363f395451305eda0bb
everm1nd <everm1nd.mail@gmail.com>
2017-07-28 15:52:00 +0200

    added constructor spec for Channel

--- test/Channel.spec.js
+++ test/Channel.spec.js
@@ -11,7 +11,7 @@ describe('<Channel/>', function () {
   const Channel = proxyquire.noCallThru().load(process.cwd() + '/src/client/app/components/Channel.jsx',
     { 'howler': { 'Howl': createHowlStub() } }
   ).default;
-  const wrapper = shallow(<Channel />);
+  const wrapper = shallow(<Channel sound='sound.wav' volume={0.5} />);
 
   it('renders one Slider', function () {
     expect(wrapper.find('Slider')).to.have.length(1);
@@ -23,5 +23,17 @@ describe('<Channel/>', function () {
 
   it('renders one SoundSelector', function () {
     expect(wrapper.find('SoundSelector')).to.have.length(1);
+  });
+
+  it('gets initialized with correct state', function () {
+    expect(wrapper.state()).to.include({
+      path: 'sound.wav',
+      volume: 0.5
+    });
+  });
+
+  it('sets volume to 0.8 if it is not set in constructor', function () {
+    const wrapper = shallow(<Channel sound='sound.wav' />);
+    expect(wrapper.state().volume).to.eql(0.8);
   });
 });
