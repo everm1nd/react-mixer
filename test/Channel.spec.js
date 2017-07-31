ba39d83ecdd060a59537611f23db3762b6f7dce3
everm1nd <everm1nd.mail@gmail.com>
2017-07-31 19:20:29 +0200

    added specs for onSoundChange function of Channel

--- test/Channel.spec.js
+++ test/Channel.spec.js
@@ -4,12 +4,14 @@ import { mount, shallow } from 'enzyme';
 const createHowlStub = () => {
   let HowlStub = sinon.stub();
   HowlStub.prototype.play = sinon.stub().returns(true);
+  HowlStub.prototype.unload = sinon.stub().returns(true);
   return HowlStub;
 };
 
 describe('<Channel/>', function () {
+  const HowlStub = createHowlStub();
   const Channel = proxyquire.noCallThru().load(process.cwd() + '/src/client/app/components/Channel.jsx',
-    { 'howler': { 'Howl': createHowlStub() } }
+    { 'howler': { 'Howl': HowlStub } }
   ).default;
   const wrapper = shallow(<Channel sound='sound.wav' volume={0.5} />);
 
@@ -35,5 +37,18 @@ describe('<Channel/>', function () {
   it('sets volume to 0.8 if it is not set in constructor', function () {
     const wrapper = shallow(<Channel sound='sound.wav' />);
     expect(wrapper.state().volume).to.eql(0.8);
+  });
+
+  context('when user changes a sound', function () {
+    const wrapper = shallow(<Channel sound='rain.wav' />);
+    wrapper.find('SoundSelector').props().onSoundChange({ target: { value: 'birds.wav' } });
+
+    it('unloads an old sound', function() {
+      expect(HowlStub.prototype.unload).to.have.been.called;
+    });
+
+    it('changes sound to a new one', function () {
+      expect(wrapper.state().path).to.eql('birds.wav');
+    })
   });
 });
