426a46bb8ad86e03db1aaa6a69c90beb0df90ef1
everm1nd <everm1nd.mail@gmail.com>
2017-08-11 17:51:36 +0200

    added test for App module

---
+++ test/App.spec.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { mount, shallow } from 'enzyme';
+
+import App from 'components/App';
+
+describe('<App/>', function () {
+  const handleSearchStub = () => {};
+  App.__Rewire__("handleSearch", handleSearchStub);
+
+  const wrapper = shallow(<App />);
+
+  it('renders a title', function () {
+    expect(wrapper.find('p').text()).to.eql('React Mixer');
+  });
+
+  it('renders two channels', function () {
+    expect(wrapper.find('Channel')).to.have.length(2);
+  });
+
+  it('renders a search field', function () {
+    expect(wrapper.find('SearchField')).to.have.length(1);
+  });
+
+  it('pass handleSearch function to SearchField', function () {
+    expect(wrapper.find('SearchField').props().onSearch).to.eql(handleSearchStub);
+  });
+});
