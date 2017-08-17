3dc85011dbbb701a25c3628a3ba9bb19eaaf8e00
everm1nd <everm1nd.mail@gmail.com>
2017-08-18 01:18:44 +0200

    changed layout a bit to add margin between buttons and sound name in Channel component

--- src/client/app/components/Channel.jsx
+++ src/client/app/components/Channel.jsx
@@ -68,8 +68,10 @@ class Channel extends React.Component {
     return (
       <div className="channel" style={style}>
         <Slider position={this.state.volume} onChange={this.onVolumeChange} />
-        <LoopButton onClick={this.toogleLoop} />
-        <SoundSwap active={this.props.inSwap} onClick={this.onSoundSwap(this)} />
+        <div className="buttons">
+          <LoopButton onClick={this.toogleLoop} />
+          <SoundSwap active={this.props.inSwap} onClick={this.onSoundSwap(this)} />
+        </div>
         <div className="sound-name">{this.state.sound.name}</div>
       </div>
     );
