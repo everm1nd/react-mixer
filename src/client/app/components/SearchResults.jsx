855ae50cbc9d4d9a1743261f15d556ce623b5aa6
everm1nd <everm1nd.mail@gmail.com>
2017-08-17 14:34:29 +0200

    rewrote SearchResults as class-component

--- src/client/app/components/SearchResults.jsx
+++ src/client/app/components/SearchResults.jsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const renderResult = (sound) => (
-  <div className="search-result" key={sound.path} data-path={sound.path}>{sound.name}</div>
-)
+class SearchResults extends React.Component {
+  renderResult(sound) {
+    return <div className="search-result" key={sound.path} data-path={sound.path}>{sound.name}</div>
+  }
 
-const renderResults = (sounds) => (
-  sounds.map((sound) => renderResult(sound))
-)
+  renderResults(sounds) {
+    return sounds.map((sound) => this.renderResult(sound))
+  }
 
-const SearchResults = (props) => {
-  return(<div className="search-results">
-    {renderResults(props.sounds)}
-  </div>);
+  render() {
+    return (
+      <div className="search-results">
+        {this.renderResults(this.props.sounds)}
+      </div>
+    );
+  }
 }
 
 SearchResults.propTypes = {
