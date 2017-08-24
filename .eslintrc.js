0b259d7fa44bc0ff9f91e884ea5f44c6f2039af5
everm1nd <everm1nd.mail@gmail.com>
2017-08-24 18:44:23 +0200

    upgraded eslint config with React settings and env for chai and so on

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,16 +12,30 @@ module.exports = {
     commonjs: true,
     es6: true,
     node: true,
+    mocha: true
   },
-  extends: ['eslint:recommended', 'plugin:react/all'],
+  extends: ["eslint:recommended", "plugin:react/all"],
   parserOptions: {
-    sourceType: 'module',
+    sourceType: "module"
   },
   rules: {
-    'linebreak-style': ['error', 'unix'],
-    quotes: ['error', 'single'],
-    semi: ['error', 'never'],
-    'no-unused-vars': ['warn'],
-    'no-console': 0,
+    "react/jsx-indent": ["warn", 2],
+    "react/jsx-no-literals": "off",
+    "react/no-set-state": "off",
+    "react/jsx-max-props-per-line": ["error", { "maximum": 3 }],
+    "react/no-unused-prop-types": "off",
+    "react/require-optimization": "off",
+    "react/jsx-no-bind": "off",
+    "react/jsx-filename-extension": "off",
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "double"],
+    semi: ["error", "never"],
+    "no-unused-vars": ["warn"],
+    "no-console": 0
   },
+  globals: {
+    "sinon": true,
+    "expect": true,
+    "proxyquire": true
+  }
 };
