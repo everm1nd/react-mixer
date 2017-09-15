7539712ac73929cbbf016c30c9bf7abd0f89d54d
Nikita Simakov <everm1nd.mail@gmail.com>
2017-09-15 16:53:14 +0200

    added pagination in SearchResults component added styles for pagination buttons pagination callback is not passed now

--- src/client/app/components/SearchResults.jsx
+++ src/client/app/components/SearchResults.jsx
@@ -1,5 +1,7 @@
 import React from "react"
 import PropTypes from "prop-types"
+import ReactPaginate from "react-paginate"
+
 import Sound from "models/Sound"
 
 import { renderDuration } from "lib/time"
@@ -25,16 +27,29 @@ class SearchResults extends React.Component {
     return sounds.map((sound) => this.renderResult(sound))
   }
 
+  renderPagination() {
+    return (
+      <ReactPaginate
+          containerClassName={"pagination"}
+          nextLabel={">"}
+          onPageChange={this.props.onPageChange}
+          previousLabel={"<"}
+      />
+    )
+  }
+
   render() {
     return (
       <div className="search-results">
         {this.renderResults(this.props.sounds)}
+        { this.props.sounds.length > 0 ? this.renderPagination() : null }
       </div>
     )
   }
 }
 
 SearchResults.propTypes = {
+  onPageChange: PropTypes.func.isRequired,
   onSelect: PropTypes.func.isRequired,
   sounds: PropTypes.arrayOf(Sound).isRequired
 }
